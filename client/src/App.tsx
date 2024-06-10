7228dabb8453042f5000b46ef509d12ee487f83b
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-06-10 10:14:43 +0200

    + Navbar, + Tailwind setup update

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import './App.css';
-import SideBar from './components/Sidebar';
+import NavBar from './components/Navbar';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,17 +9,17 @@ function App() {
     const response = await fetch('http://localhost:5022/api/test');
     const result = await response.json();
     console.log('result :>> ', result);
-
-    useEffect(() => {
-      fetchServer();
-    }, []);
-
-    return (
-      <div>
-        <h1 className='text-3xl font-bold underline'>Hello world!</h1>
-        <SideBar />
-      </div>
-    );
   };
+
+  useEffect(() => {
+    fetchServer();
+  }, []);
+
+  return (
+    <div>
+      <NavBar />
+    </div>
+  );
 }
+
 export default App;
--- client/src/index.tsx
+++
@@ -1,4 +0,0 @@
-import './index.css';
-
-// mongodb+srv://vkyslik:<password>@cluster0.3tbv0z4.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-// ISjS9joGifmeGGMr
