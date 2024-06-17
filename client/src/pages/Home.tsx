dd90d645cc92c3ef47509461e210d39cf72dea4e
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-06-17 15:28:14 +0200

    home container centered

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -7,7 +7,7 @@ const Home = () => {
 
   return (
     <div className='relative inset-0 z-10 flex items-center justify-center'>
-      <div className='bg-white max-w-sm p-20 rounded-lg shadow-lg text-center'>
+      <div className='bg-white max-w-sm p-10 mt-64 rounded-lg shadow-lg text-center'>
         <h1 className='text-2xl font-bold mb-4'>Welcome to My Website</h1>
         <p className='mt-4'>This is the content of the page.</p>
       </div>
