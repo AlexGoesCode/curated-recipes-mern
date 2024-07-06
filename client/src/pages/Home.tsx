8ebc1dcb4486033a046e6e91814b648639b759f6
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-07-06 19:53:31 +0200

    Home page intro made

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -7,9 +7,16 @@ const Home = () => {
 
   return (
     <div className='relative inset-0 z-10 flex items-center justify-center'>
-      <div className='bg-white max-w-sm p-10 mt-64 rounded-lg shadow-lg text-center'>
-        <h1 className='text-2xl font-bold mb-4'>Welcome to My Website</h1>
-        <p className='mt-4'>This is the content of the page.</p>
+      <div className='bg-white max-w-2xl h-auto p-10 mt-40 rounded-2xl shadow-lg text-center'>
+        <h1 className='text-2xl font-bold mb-4'>
+          Welcome to Curated Recipes Database
+        </h1>
+        <p className='mt-4 text-xl'>
+          Our curated database features an extensive collection of recipes
+          spanning various cuisines, dietary preferences, and skill levels.
+          Whether you are a novice cook or a seasoned chef, you'll find
+          inspiration and guidance to create mouth-watering dishes that impress.
+        </p>
       </div>
     </div>
   );
