2fac597dc3899bdddf38b3cb9fbe5c9e21b5ef43
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-07-07 15:44:30 +0200

    few tweaks with navbar and searchbar

--- client/src/components/ProtectedRoute.tsx
+++ client/src/components/ProtectedRoute.tsx
@@ -10,7 +10,6 @@ const ProtectedRoute = ({ children }: ProtectedRouteProps) => {
   const { user, isLoading } = useAuth(); // Assuming [`useAuth`](command:_github.copilot.openSymbolFromReferences?%5B%7B%22%24mid%22%3A1%2C%22fsPath%22%3A%22%2FUsers%2Falex%2Freact-app%2Fflavours-dance%2Fclient%2Fsrc%2Fcontext%2FAuthContext.tsx%22%2C%22external%22%3A%22file%3A%2F%2F%2FUsers%2Falex%2Freact-app%2Fflavours-dance%2Fclient%2Fsrc%2Fcontext%2FAuthContext.tsx%22%2C%22path%22%3A%22%2FUsers%2Falex%2Freact-app%2Fflavours-dance%2Fclient%2Fsrc%2Fcontext%2FAuthContext.tsx%22%2C%22scheme%22%3A%22file%22%7D%2C%7B%22line%22%3A107%2C%22character%22%3A0%7D%5D "client/src/context/AuthContext.tsx") now also provides a token
 
   if (isLoading) {
-    //! create a component in component folder called Spinner.tsx , and use it here
     return (
       <div role='status'>
         <svg
--- client/src/components/SearchBar.tsx
+++ client/src/components/SearchBar.tsx
@@ -37,11 +37,11 @@ function SearchBar({
   }
 
   return (
-    <form className='max-w-xl mx-auto my-8' onSubmit={handleSubmit}>
+    <form className='max-w-xl mx-auto my-8 relative' onSubmit={handleSubmit}>
       <div className='flex'>
         <select
           id='dropdown-button'
-          className='z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600'
+          className='z-10 inline-flex items-center py-2.5 px-4 text-sm font-medium text-center text-gray-900 bg-gray-100 border border-gray-300 rounded-l-lg hover:bg-gray-200 focus:ring-4 focus:outline-none focus:ring-gray-100 dark:bg-gray-700 dark:hover:bg-gray-600 dark:focus:ring-gray-700 dark:text-white dark:border-gray-600 relative'
           value={searchOption}
           onChange={handleOptionChange}
         >
@@ -54,13 +54,13 @@ function SearchBar({
           type='search'
           id='search-dropdown'
           onChange={handleInputChange}
-          className='block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 border-gray-300 rounded-r-none focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500'
+          className='relative block p-2.5 w-full z-20 text-sm text-gray-900 bg-gray-50 border-gray-300 rounded-r-none focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:border-blue-500'
           placeholder='Search...'
           required
         />
         <button
           type='submit'
-          className='p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
+          className='relative p-2.5 text-sm font-medium text-white bg-blue-700 rounded-r-lg border border-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800'
         >
           <svg
             className='w-4 h-4'
