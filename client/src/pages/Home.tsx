df5987c650b20cb2d161618779f86d231a7e6c1c
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-09-24 18:34:37 +0200

    styles

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,4 +1,4 @@
-import { useContext } from 'react';
+// import { useContext } from 'react';
 // import { AuthContext, useAuth } from '../../context/AuthContext';
 
 const Home = () => {
@@ -7,11 +7,11 @@ const Home = () => {
 
   return (
     <div className='relative inset-0 z-10 flex items-center justify-center'>
-      <div className='bg-white max-w-2xl h-auto p-10 mt-40 rounded-2xl shadow-lg text-center'>
-        <h1 className='text-2xl font-bold mb-4'>
+      <div className='bg-gray-900 bg-opacity-60 max-w-2xl h-auto p-10 mt-40 border rounded-2xl shadow-lg text-center'>
+        <h1 className='text-2xl font-bold mb-4 text-gray-100'>
           Welcome to Curated Recipes Database
         </h1>
-        <p className='mt-4 text-xl'>
+        <p className='mt-4 text-xl text-gray-100'>
           Our curated database features an extensive collection of recipes
           spanning various cuisines, dietary preferences, and skill levels.
           Whether you are a novice cook or a seasoned chef, you'll find
