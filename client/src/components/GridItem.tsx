7ca920d1f5beeaa1eedf3289cd1242ec696c6a72
Vaclav "Alex" Kyslik <v.kyslik@gmail.com>
2024-09-24 18:32:17 +0200

    added memo

--- client/src/components/GridItem.tsx
+++ client/src/components/GridItem.tsx
@@ -1,7 +1,8 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { Link } from 'react-router-dom';
 import { Recipe } from '../types/Types';
 import LikeButton from './LikeButton';
 import { useAuth } from '../context/AuthContext';
+import { memo } from 'react';
 
 interface GridItemProps {
   item: Recipe;
@@ -9,11 +10,11 @@ interface GridItemProps {
   fetchData: () => Promise<void>;
 }
 
-const GridItem = ({ item, isLiked, fetchData }: GridItemProps) => {
+const GridItem = memo(({ item, isLiked, fetchData }: GridItemProps) => {
   const { user } = useAuth();
 
   return (
-    <div className='relative bg-gray-900 bg-opacity-50 w-56 p-4 border rounded-2xl shadow-md cursor-pointer'>
+    <div className='relative bg-gray-900 bg-opacity-60 w-56 p-4 border rounded-2xl shadow-md cursor-pointer'>
       {item.picture && (
         <Link to={`/recipes/${item._id}`}>
           <img
@@ -27,22 +28,17 @@ const GridItem = ({ item, isLiked, fetchData }: GridItemProps) => {
       <p className='text-gray-100'>{item.origin}</p>
       <p className='text-gray-100'>{item.likes?.length} Likes</p>
       <p className='text-gray-100'>
-        Diet:{' '}
-        {Array.isArray(item.diet) && item.diet.length > 0
-          ? item.diet.join(', ')
-          : 'none'}
+        Diet: {Array.isArray(item.diet) ? item.diet.join(', ') : 'none'}
       </p>
-      <div className='absolute bottom-2 right-2 '>
+      <div className='absolute bottom-2 right-2'>
         <LikeButton
           recipeId={item._id}
-          // userId={'your-user-id'} // Replace with dynamic user ID if available
           isLiked={isLiked}
-          // Pass down the handleLike function
           fetchData={fetchData}
         />
       </div>
     </div>
   );
-};
+});
 
 export default GridItem;
